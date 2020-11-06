f8915ec07ecc674afdedd7666e08352a75265541
catse2000 <catse2000@yahoo.com>
2020-11-05 19:00:21 -0600

    populate models, create relationships

---
+++ models/Lesson.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes, TEXT } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Lesson extends Model {};
+
+Lesson.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                min: 1, 
+                max: 160
+            }
+        },
+        desc: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                min: 1, 
+                max: 2000
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        topic_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'topic',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: false,
+        modelName: 'lesson'
+    }
+);
+
+module.exports = Lesson;
--- models/index.js
+++ models/index.js
@@ -1,29 +1,68 @@
 const User = require("./User");
-const Post = require("./Post");
-// const Comment = require("./Comment");
+const Topic = require('./Topic');
+const Lesson = require("./Lesson");
+const Vote = require('./Vote');
+const Comment = require("./Comment");
 
-// User.hasMany(Post, {
-//     foreignKey: 'user_id',
-// });
+// create relationships
+User.hasMany(Lesson, {
+    foreignKey: 'user_id'
+});
 
-// User.hasMany(Comment, {
-//     foreignKey: 'user_id'
-// });
+Lesson.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-// Post.belongsTo(User, {
-//     foreignKey: 'user_id',
-// });
+User.belongsToMany(Lesson, {
+    through: Vote, 
+    as: 'voted_lessons',
+    foreignKey: 'user_id'
+});
 
-// Post.hasMany(Comment, {
-//     foreignKey: 'post_id'
-// });
+Lesson.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_lessons',
+    foreignKey: 'lesson_id'
+});
 
-// Comment.belongsTo(User, {
-//     foreignKey: 'user_id'
-// });
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-// Comment.belongsTo(Post, {
-//     foreignKey: 'post_id'
-// });
+Vote.belongsTo(Lesson, {
+    foreignKey: 'lesson_id'
+});
 
-module.exports = { User, Post };
+User.hasMany(Vote, {
+    foriegnKey: 'user_id'
+});
+
+Lesson.hasMany(Vote, {
+    foreignKey: 'lesson_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Lesson, {
+    foreignKey: 'lesson_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Lesson.hasMany(Comment, {
+    foreignKey: 'lesson_id'
+});
+
+Topic.hasMany(Lesson, {
+    foreignKey: 'topic_id'
+});
+
+Lesson.belongsTo(Topic, {
+    foreignKey: 'topic_id'
+});
+
+module.exports = { User, Topic, Lesson, Vote, Comment };

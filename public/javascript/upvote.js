01019a2f02785aeca7542d902655582ac1d0de43
Ender <woodwind.turbeville@gmail.com>
2020-11-12 22:28:08 -0600

    Added new Constant in attempt to fix Vote issue in Heroku

--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -1,14 +1,14 @@
 async function upvoteClickHandler(event) {
     event.preventDefault();
   
-    const id = window.location.toString().split('/')[
+    const lesson_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
   
     const response = await fetch('/api/lessons/upvote', {
         method: 'PUT',
         body: JSON.stringify({
-          lesson_id: id
+          lesson_id,
         }),
         headers: {
           'Content-Type': 'application/json'
